7ba176685541fd6b2938254111f52d21a9b484f7
emjds <jacob@jacobsbin.com>
2017-04-12 19:25:46 -0500

    changed momentum values to make controls a little less floaty

--- src/game.lua
+++ src/game.lua
@@ -1,8 +1,8 @@
 local game = {}
 game.step = 10000
 game.friction = 0.146875 * game.step
-game.accel = 0.046875 * game.step
-game.deccel = 0.5 * game.step
+game.accel = 0.1 * game.step
+game.deccel = .9 * game.step
 game.gravity = 0.21875 * game.step
 game.fall_grace = .075
 game.fall_dps = 15
